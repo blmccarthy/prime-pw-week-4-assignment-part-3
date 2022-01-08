54be5b3967012908a476cc46b58c532377f02696
Brant McCarthy <brant.mccarthy@me.com>
2022-01-08 13:24:41 -0600

    finished stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,8 +5,14 @@ console.log('***** Cart Functions *****');
 let basket = [];
 
 function addItem(item) {
-  basket.push(item);
-  return true;
+  if (isFull() === false){
+    basket.push(item);
+    return true;
+  }
+  else if (isFull()){
+    console.log(`CART FULL! Unable to add ${item} to cart`);
+    return false;
+  }
 }
 
 function listItems() {
@@ -40,4 +46,42 @@ empty();
 listItems();
 
 console.log("I'm adding a Prebuilt PC to cart (expect true):", addItem('Prebuilt PC'));
+listItems();
+
+/// Stretch Goals ///
+
+const maxItems = 5;
+basket = [];
+
+function isFull(){
+  if (basket.length < 5){
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+console.log("I'm adding a item_01 to cart (expect true):", addItem('item_01'));
+console.log("I'm adding a item_02 to cart (expect true):", addItem('item_02'));
+console.log("I'm adding a item_03 to cart (expect true):", addItem('item_03'));
+console.log("I'm adding a item_04 to cart (expect true):", addItem('item_04'));
+console.log("I'm adding a item_05 to cart (expect true):", addItem('item_05'));
+console.log("I'm adding a item_06 to cart (expect true):", addItem('item_06'));
+listItems();
+
+function removeItem(item){
+  if (basket.indexOf(item) > -1){             // checks for item string in basket array
+    basket.splice(basket.indexOf(item), 1);   // if found, it removes from basket array
+    return true;
+  }
+  else {
+    console.log(`NULL: ${item} was not found in your basket`);  // if item isn't found, function reports item wasn't found
+    return false;
+  }
+}
+
+console.log('remove item_04 from cart:', removeItem('item_04'));
+listItems();
+console.log('remove item_04 from cart:', removeItem('item_04'));
 listItems();
